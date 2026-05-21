e435050a9d08ed669ec3aa26dc615facaf329784
Venkatesh Narasimha <venkatesh@jatramela.in>
2026-05-21 14:05:36 +0530

    Remove featured products from home page

--- apps/storefront/src/app/[countryCode]/(main)/page.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from "next"
 
-import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
-import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
@@ -20,22 +18,13 @@ export default async function Home(props: {
 
   const region = await getRegion(countryCode)
 
-  const { collections } = await listCollections({
-    fields: "id, handle, title",
-  })
-
-  if (!collections || !region) {
+  if (!region) {
     return null
   }
 
   return (
     <>
       <Hero />
-      <div className="py-12">
-        <ul className="flex flex-col gap-x-6">
-          <FeaturedProducts collections={collections} region={region} />
-        </ul>
-      </div>
     </>
   )
 }
